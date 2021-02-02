1471b22952ac06cbcca244fb6539c2961c34be27
unknown <comewithme1200@gmail.com>
2021-02-02 15:15:27 +0700

    intent2

--- Buoi1onl/app/src/main/java/com/example/buoi1onl/MainActivity.java
+++ Buoi1onl/app/src/main/java/com/example/buoi1onl/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView image;
+    ImageView image,image1,image2,image3;
     EditText txtname;
     EditText txtage;
     Spinner spinnerhome;
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     int age;
     String name;
     String spinner;
-
+    int id;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +35,40 @@ public class MainActivity extends AppCompatActivity {
         spinnerhome = (Spinner)findViewById(R.id.spinnerhome);
         sendbtn = (Button)findViewById(R.id.Btnsend);
         image.setImageResource(R.drawable.profile);
+        image1 = (ImageView)findViewById(R.id.imageView3);
+        image2 = (ImageView)findViewById(R.id.imageView4);
+        image3 = (ImageView)findViewById(R.id.imageView5);
         ArrayList<String> dsque = new ArrayList<>();
         dsque.add("Ha Noi");
         dsque.add("TPHCM");
         dsque.add("Hải Phòng");
         dsque.add("Đà Nẵng");
         dsque.add("Cần Thơ");
+        final ArrayList<Integer> dsanh = new ArrayList<>();
+        dsanh.add(R.drawable.profile1);
+        dsanh.add(R.drawable.profile2);
+        dsanh.add(R.drawable.profile3);
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = dsanh.get(0);
+                image.setImageResource(id);
+            }
+        });
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = dsanh.get(1);
+                image.setImageResource(id);
+            }
+        });
+        image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = dsanh.get(2);
+                image.setImageResource(id);
+            }
+        });
         ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.support_simple_spinner_dropdown_item,dsque);
         spinnerhome.setAdapter(adapter);
         sendbtn.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                     age = Integer.parseInt(txtage.getText().toString());
                     name = txtname.getText().toString();
                     spinner = spinnerhome.getSelectedItem().toString();
+                    id = id;
                 } catch (Exception e) {
 
                 }
@@ -63,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
                 Mybundle.putInt("age", age);
                 Mybundle.putString("name", name);
                 Mybundle.putString("home",spinner);
-                Mybundle.putInt("image",R.drawable.profile);
+                Mybundle.putInt("image",id);
+
                 //đưa bundle vào intent
                 MyFintent.putExtra("data", Mybundle);
                 startActivity(MyFintent);
